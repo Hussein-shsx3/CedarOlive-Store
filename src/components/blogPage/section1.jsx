964c03b4dcc4535bc8c35daba1abb79e4bc06638
Hussein-shsx3 <husseinshsx3@gmail.com>
2025-03-19 15:23:16 +0200

    edit

--- src/components/blogPage/section1.jsx
+++ src/components/blogPage/section1.jsx
@@ -4,7 +4,7 @@ const Section1 = () => {
   return (
     <section className="w-full container flex flex-col items-start mt-14">
       <div className="w-full my-5 px-5 md:px-0">
-        <div className="flex flex-col md:flex-row bg-[#131313] justify-center items-stretch overflow-hidden h-auto min-h-[450px]">
+        <div className="flex flex-col md:flex-row bg-[#131313] justify-between items-stretch overflow-hidden h-auto min-h-[450px]">
           {/* Left - Text Block */}
           <div className="w-full md:w-[38%] text-white p-6 sm:p-8 md:p-10 flex flex-col justify-start space-y-4">
             <p className="text-sm text-text">July 29, 2022</p>
@@ -14,7 +14,7 @@ const Section1 = () => {
             <button className="w-[30%] py-3 bg-gray-700">Read More</button>
           </div>
           {/* Right - Main Image */}
-          <div className="w-full md:w-[62%] h-48 sm:h-64 md:h-full">
+          <div className="w-full md:w-[62%] md:h-full">
             <img
               src="/Images/62d85703aeb2f6707c3ab42c_thumbnail three-p-800.jpeg"
               alt="Living Room Decor"
